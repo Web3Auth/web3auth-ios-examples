4801fc8d9fe5c10c7c3d84941d5669591c2dcc3c
Maharshi Mishra <ihsraham27@gmail.com>
2025-11-05 13:35:34 +0530

    Updated Ios playground example

--- ios-playground/ios-playground/Helpers/Web3AuthHelper.swift
+++ ios-playground/ios-playground/Helpers/Web3AuthHelper.swift
@@ -7,23 +7,30 @@
 
 import Foundation
 import Web3Auth
+import FetchNodeDetails
 
 class Web3AuthHelper {
     
     var web3Auth: Web3Auth!
     
+    private let clientID = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ"
+    private let redirectUrl = "com.w3a.ios-playground://auth"
+    private let web3AuthNetwork: Web3AuthNetwork = .SAPPHIRE_MAINNET
+    private let buildEnv: BuildEnv = .production
+    
     func initialize() async throws {
         web3Auth = try await Web3Auth(
-            W3AInitParams(
-                clientId: "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ",
-                network: Network.sapphire_mainnet,
-                redirectUrl: "com.w3a.ios-playground://auth"
+            options: .init(
+                clientId: clientID,
+                redirectUrl: redirectUrl,
+                authBuildEnv: buildEnv,
+                web3AuthNetwork: web3AuthNetwork
             )
         )
     }
     
     func isUserAuthenticated() -> Bool {
-        return web3Auth.state != nil
+        return web3Auth.web3AuthResponse != nil
     }
     
     func logOut() async throws {
@@ -34,18 +41,24 @@ class Web3AuthHelper {
         return try web3Auth.getUserInfo()
     }
     
+    func getPrivateKey() -> String {
+        return web3Auth.getPrivateKey()
+    }
+    
     func getSolanaPrivateKey() throws -> String {
-        return web3Auth.getEd25519PrivKey()
+        return try web3Auth.getEd25519PrivateKey()
+    }
+    
+    func getWeb3AuthResponse() -> Web3AuthResponse? {
+        return web3Auth.web3AuthResponse
     }
     
     func login(email: String) async throws {
-        let _ = try await web3Auth.login(
-            W3ALoginParams(
-                loginProvider: Web3AuthProvider.EMAIL_PASSWORDLESS,
+        _ = try await web3Auth.login(
+            loginParams: LoginParams(
+                authConnection: .EMAIL_PASSWORDLESS,
                 extraLoginOptions: ExtraLoginOptions(login_hint: email)
             )
         )
-        
-        return
     }
 }
